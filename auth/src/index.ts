bf2096cf6666970a262a2f8e2c628ca2a37dd2ae
Pramuditha Jayasinghe <pramuditha.niroshan@circles.asia>
2023-10-31 21:27:29 +0530

    Add getUser route and ingress nginx configs

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,4 +5,8 @@ const app = express()
 const port = 3000
 app.use(json)
 
+app.get('/api/users/getCurrentUser', (req, res) => {
+    res.send("its working!!!")
+})
+
 app.listen(port, () => console.log(`listening on port ${port}!!!!!!`))
