4af156baecffbbaf0da12f9eeecf538c800e8c7b
Pramuditha Jayasinghe <pramuditha.niroshan@circles.asia>
2023-11-02 22:46:47 +0530

    add express-validator and validate request body params for signup request

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,31 @@
-import express from 'express';
-const  router = express.Router();
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
 
-router.post('/api/users/signup', (req, res) => {
-    res.send("hi there!")
-});
+const router = express.Router();
 
-export { router as signUpRouter}
+router.post(
+    "/api/users/signup",
+    [
+        body("email").isEmail().withMessage("Email must be valid"),
+        body("password")
+            .trim()
+            .isLength({ min: 4, max: 20 })
+            .withMessage("Password must be between 4 and 20 characters"),
+    ],
+    (req: Request, res: Response) => {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(400).send(errors.array());
+        }
+        const { email, password } = req.body;
+
+        console.log("Creating a user...");
+
+        res.send({});
+
+        // new User({ email, password })
+    }
+);
+
+export { router as signUpRouter }
