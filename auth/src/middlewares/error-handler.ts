effd232e4f098343a50b56de68bfb92b1cfcb582
Pramuditha Jayasinghe <pramuditha.niroshan@circles.asia>
2023-11-03 14:48:09 +0530

    Determine the error type

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,8 +1,18 @@
 import { NextFunction, Request, Response } from "express";
+import { RequestValidationError } from "../errors/request-validation-errors";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
 
 export const erorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-    console.log("someting went wrong!", err);
+
+    if (err instanceof RequestValidationError) {
+        console.log("handling this error as a request validation error");
+    }
+
+    if (err instanceof DatabaseConnectionError) {
+        console.log("handling this error as a db connection error");
+    }
+
     res.status(400).send({
-        message: "Someting went wrong!"
+        message: err.message,
     })
 }
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import { RequestValidationError } from "../errors/request-validation-errors";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
 
 const router = express.Router();
 
@@ -16,11 +18,12 @@ router.post(
         const errors = validationResult(req);
 
         if (!errors.isEmpty()) {
-            throw new Error('Invalid password or email!')
+            throw new RequestValidationError(errors.array())
         }
         const { email, password } = req.body;
 
         console.log("Creating a user...");
+        throw new DatabaseConnectionError()
 
         res.send({});
 
