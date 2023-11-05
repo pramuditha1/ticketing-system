95a3afe42e147267030b181d4021f3e02e6536f0
Pramuditha Jayasinghe <pramuditha.niroshan@circles.asia>
2023-11-05 09:16:49 +0530

    Modify error handler to show express-validator errors

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -3,16 +3,19 @@ import { RequestValidationError } from "../errors/request-validation-errors";
 import { DatabaseConnectionError } from "../errors/database-connection-error";
 
 export const erorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-
     if (err instanceof RequestValidationError) {
-        console.log("handling this error as a request validation error");
-    }
-
-    if (err instanceof DatabaseConnectionError) {
-        console.log("handling this error as a db connection error");
-    }
-
-    res.status(400).send({
-        message: err.message,
-    })
+        const formattedErrors = err.errors.map((error) => {
+          if (error.type === 'field') {
+            return { message: error.msg, field: error.path };
+          }
+        });
+        return res.status(400).send({ errors: formattedErrors });
+      }
+      if (err instanceof DatabaseConnectionError) {
+        return res.status(500).send({ errors: [{ message: err.reason }] });
+      }
+    
+      res.status(400).send({
+        errors: [{ message: 'Something went wrong' }],
+      });
 }
